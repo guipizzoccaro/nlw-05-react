0e9cf4297f493afdc77f7b3bc55abb4ba18babfa
Guilherme Pizzoccaro <guilherme.pizzoccaro@outlook.com>
2021-04-24 23:22:28 -0300

    4rd class finalized

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
+import { useContext } from 'react'
 import { GetStaticProps } from 'next';
 import Image from 'next/image'
+import Link from 'next/link'
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
-import Link from 'next/link'
+import { PlayerContext } from '../contexts/playerContext';
 
 import styles from './home.module.scss'
-
 
 //SPA
 //SSR
@@ -31,6 +33,7 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext)
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -57,7 +60,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
